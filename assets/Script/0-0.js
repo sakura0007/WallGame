b924d9a34d3fbc46385882bc76454b48d76bbbfa
sakura007 <35498776+sakura0007@users.noreply.github.com>
2020-06-01 13:27:17 +0800

    项目第一次上传

---
+++ assets/Script/0-0.js
@@ -0,0 +1,34 @@
+﻿cc.Class({
+    extends: cc.Component,
+
+    properties: {
+
+    },
+
+    onLoad() {
+
+    },
+
+    start() {
+        cc.director.preloadScene("Scene/1-0");
+        this.tryone = true;
+    },
+
+    Start() {
+        if (this.tryone) {
+            this.tryone = false;
+            //音效
+            cc.find('Canvas/click3bgm').getComponent(cc.AudioSource).play();
+            this.scheduleOnce(function () {
+                cc.director.loadScene("Scene/1-0");
+            }, 0.5);
+        }
+    },
+
+    GameOff() {
+        //音效
+        cc.find('Canvas/click3bgm').getComponent(cc.AudioSource).play();
+        cc.game.end();
+    }
+    // update (dt) {},
+});
